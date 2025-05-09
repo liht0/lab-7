ff887c3885172a23b975275b0249979d2e05405d
liht0 <liht0yager@gmail.com>
2025-05-09 22:24:52 +1000

    main.c

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
-#include <>
+
 
 struct humen{
 
 	char * name;
 	char * surname;
 	int age;
-	};
+	char * proverka;
+
+
 int main(){
 	humen massive_1[4];
 	humen massive_2[4];
-	
-	
 
+	printf("vvedite imiya: /n");
+	scanf("%s"; &name);
+	printf("vvedite surname: /n");
+	scanf("%s"; &name);
+	printf("vvedite age: /n");
+	scanf("%ld"; &name);
+	printf("prodolzhit?(y\n)");
+	scanf("%s"; proverka);
+	if (proverka = n) break;
+	
 }
-
